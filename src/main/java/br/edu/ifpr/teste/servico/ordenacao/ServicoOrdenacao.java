ba1d20227b48275478384df4b7a50d331be7ba20
Marcelo F. Terenciani <marcelo.terenciani@gmail.com>
2025-04-08 17:28:36 -0300

    Exemplo de serviço de ordenação básico - com teste

---
+++ src/main/java/br/edu/ifpr/teste/servico/ordenacao/ServicoOrdenacao.java
@@ -0,0 +1,44 @@
+package br.edu.ifpr.teste.servico.ordenacao;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ServicoOrdenacao {
+
+    public ArrayList<Integer> obterDadosArquivo(String nomeArquivo) throws Exception {
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(nomeArquivo);
+
+        ArrayList<Integer> lista = new ArrayList<>();
+
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            String primeiraLinha = reader.readLine();
+            int quantidadeDeNumeros = Integer.parseInt(primeiraLinha);
+
+            for (int i = 0; i < quantidadeDeNumeros; i++) {
+                String linha = reader.readLine();
+                int numero = Integer.parseInt(linha);
+                lista.add(numero);
+            }
+        }
+        return lista;
+    }
+
+    public ArrayList<Integer> ordenar(ArrayList<Integer> original) {
+        ArrayList<Integer> ordenado = new ArrayList<>(original);
+        Collections.sort(ordenado);
+
+        return ordenado;
+    }
+
+    public String prepararParaImpressao(ArrayList<Integer> ordenado) {
+        String saida = ordenado.get(0).toString();
+        for (int i = 1; i < ordenado.size(); i++) {
+            saida += ", " + ordenado.get(i);
+        }
+
+        return saida;
+    }
+}
---
+++ src/test/java/br/edu/ifpr/teste/servico/ordenacao/TesteOrdenacao.java
@@ -0,0 +1,39 @@
+package br.edu.ifpr.teste.servico.ordenacao;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+public class TesteOrdenacao {
+
+    @Test
+    public void deveOrdenar() throws Exception {
+        // Arrange (PREPARAR) - Roy Osherove no livro “The Art of Unit Testing”.
+        // Given (DADO) - BDD (Behavior Driven Development)
+
+        String nomeArquivo = "casoUm.txt";
+        String entradaEsperada = "5, 4, 6";
+        String saidaEsperada = "4, 5, 6";
+
+        InputStream inputStream = getClass().getClassLoader().getResourceAsStream(nomeArquivo);
+
+        assertNotNull(inputStream, "Arquivo casoUm.txt não encontrado em test/resources");
+        // Act (AGIR)
+        // When (QUANDO)
+        ServicoOrdenacao servicoOrdenacao = new ServicoOrdenacao();
+        ArrayList<Integer> original = servicoOrdenacao.obterDadosArquivo(nomeArquivo);
+        ArrayList<Integer> ordenado = servicoOrdenacao.ordenar(original);
+        String entrada = servicoOrdenacao.prepararParaImpressao(original);
+        String saida = servicoOrdenacao.prepararParaImpressao(ordenado);
+
+        // Assert (VERIFICAR)
+        // Then (ENTÃO)
+        assertEquals(entradaEsperada, entrada, "Entrada não está formatada corretamente");
+        assertEquals(saidaEsperada, saida, "Saída não está formatada corretamente");
+    }
+
+}
